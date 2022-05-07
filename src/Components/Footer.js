202fdb83f0b65894e2b772bb17b7bdc11257eccf
Valeriia Diachenko <Valerusya55@mail.ru>
2022-05-07 10:50:18 +0600

    master

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,35 +2,35 @@ import React, { Component } from 'react'
 import Contacts from '../Pages/Contacts'
 
 export default class Footerooter extends Component {
-  render() {
-    return (
-      <div className='Footer'>
-          <div className='line'></div>
-          <div className='FooterBlock'>
-              <ul>
-              <h5>Покупателям</h5>
-                  <li><a href=''>Гарантия</a></li>
-                  <li><a href=''>Как сделать заказ</a></li>
-                  <li><a href=''>Возврат товара</a></li>
-                  <li><a href=''>Возврат денежных средств</a></li>
-              </ul>
-          </div>
-          <div className='FooterBlock' text-align = "center">
-          <h5>Оставайтесь на связи</h5>
-              <a href = 'https://t.me/+79040725414'><img src='telega.png'></img></a>
-              <a href = 'https://wa.me/79040725414?text=Здравствуйте%2C+у+меня+есть+вопрос'><img src='whatsup.png'></img></a>
-          </div>
-          <div className='FooterBlock'>
-          <ul>
-              <h5>Наши контакты</h5>
-                  <li><a href='tel:+73812551381'><img src='phone.png'></img>+7(3812)55‒13‒81</a></li>
-                  <li><a href='mailto:agrozapchast55@mail.ru'><img src='mail.png'></img>agrozapchast55@mail.ru</a></li>
-                  <li><p2><img src='geo.png'></img>Омская область, город Омск,<br/> улица Семиреченская, 97А, корпус 3</p2></li>
-              </ul>
-          </div>
-          <hr/>
-          <p1>&copy; 2022 Все права защищены и принадлежат ООО «АгроЗапчасть» </p1>
-          </div>
-    )
-  }
+    render() {
+        return (
+            <div className='Footer'>
+                <div className='line'></div>
+                <div className='FooterBlock'>
+                    <ul>
+                        <h5>Покупателям</h5>
+                        <li><a href=''>Гарантия</a></li>
+                        <li><a href=''>Как сделать заказ</a></li>
+                        <li><a href=''>Возврат товара</a></li>
+                        <li><a href=''>Возврат денежных средств</a></li>
+                    </ul>
+                </div>
+                <div className='FooterBlock' text-align="center">
+                    <h5>Оставайтесь на связи</h5>
+                    <a href='https://t.me/+79040725414'><img src='telega.png'></img></a>
+                    <a href='https://wa.me/79040725414?text=Здравствуйте%2C+у+меня+есть+вопрос'><img src='whatsup.png'></img></a>
+                </div>
+                <div className='FooterBlock'>
+                    <ul>
+                        <h5>Наши контакты</h5>
+                        <li><a href='tel:+73812551381'><img src='phone.png'></img>+7(3812)55‒13‒81</a></li>
+                        <li><a href='mailto:agrozapchast55@mail.ru'><img src='mail.png'></img>agrozapchast55@mail.ru</a></li>
+                        <li><p><img src='geo.png'></img>Омская область, город Омск,<br /> улица Семиреченская, 97А, корпус 3</p></li>
+                    </ul>
+                </div>
+                <hr />
+                <p>&copy; 2022 Все права защищены и принадлежат ООО «АгроЗапчасть» </p>
+            </div>
+        )
+    }
 }
--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 
+
 export default class Contacts extends Component {
   render() {
     return (
-      <div>Contacts</div>
+      <div className='ContactBlock'>
+
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A6f9cfb64e307617ace86b6c428e72845db804c3cf85bffa463a871d78af815d1&amp;source=constructor" width="100%" height="488" frameBorder="0"></iframe>
+
+        <div className='contact'>
+          <img src='about.png'></img>
+        </div>
+        <div className='contactMiddle'>
+          <p>644016, Омск, улица Семиреченская, 97А, корпус 3 </p>
+          <img src='clock.png'></img>
+          <p>Пн - Пт: 9.00 - 18.00 <br /> Сб, Вс: выходной</p>
+        </div>
+        <div className='contactBottom'>
+          <div className='rectangleOne'>
+            <img src='nal.png'></img>
+            <p>Наличные</p>
+          </div>
+          <div className='rectangleTwo'>
+          <img src='beznal.png'></img>
+          <p>Безналичный расчет</p>
+          </div>
+        </div>
+      </div>
     )
   }
 }
