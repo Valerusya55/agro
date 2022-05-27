62bccac9ca513d397e73b05ea4adf7cdc5777866
Valeriia Diachenko <Valerusya55@mail.ru>
2022-05-27 16:42:51 +0600

    master

--- src/Pages/Basket.js
+++ src/Pages/Basket.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react'
 
 export default class Basket extends Component {
-  render() {
-    return (
-        <div className="row">
-            <div className="col-lg-8 col-md-8 col-xs-8 thumb">
-                <div className='basketProduct'>
-                    <h1>ТОВАРЫ</h1>
-                    <div className='lineSearch'></div>
-                    <img src='tovar.jpg'></img>
-                    <a href=''>Название товара</a>
-                    <div className='lineSearch'></div>
-                    <img src='tovar.jpg'></img>
-                    <a href=''>Название товара</a>
-                    <div className='lineSearch'></div>
-                    <img src='tovar.jpg'></img>
-                    <a href=''>Название товара</a>
+    render() {
+        return (
+            <div className="row">
+                <div className="col-lg-8 col-md-8 col-xs-8 thumb">
+                    <div className='basketProduct'>
+                        <h1>ТОВАРЫ</h1>
+                        <div className='lineSearch'></div>
+                        <img src='tovar.jpg'></img>
+                        <a href=''>Название товара</a>
+                        <div className='lineSearch'></div>
+                        <img src='tovar.jpg'></img>
+                        <a href=''>Название товара</a>
+                        <div className='lineSearch'></div>
+                        <img src='tovar.jpg'></img>
+                        <a href=''>Название товара</a>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-4 thumb">
+                    <div className='orderPrice'>
+                        <h1>ИТОГО</h1>
+                        <div className='lineSearch'></div>
+                        <p>Товары, 2 шт.</p>
+                        <p>Заказ будет готов 24.02.2022</p>
+                        <div className='btnOrder'>
+                            <a href='#' type="submit">Заказать</a>
+                        </div>
+                        <img src='sogl.png'></img>
+                        <p>Согласен с условиями Правил пользования торговой площадкой и правилами возврата</p>
+                    </div>
                 </div>
             </div>
-            <div className="col-lg-4 col-md-4 col-xs-4 thumb">
-                <div className='order'>
-                    <p>ЗАКАЗЫ</p>
-                    <select className="select" data-mdb-filter="true">
-                        <option defaultValue>Все</option>
-                        <option value="1">Активные</option>
-                        <option value="2">Завершенные</option>
-                        <option value="3">Отмененные</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-    )
-  }
+        )
+    }
 }
